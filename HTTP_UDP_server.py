6a2e7f9daac901ca40395f50737c26e2c069abd0
Jeevan <78906553+daj337n96@users.noreply.github.com>
2021-04-10 18:18:41 +0800

    Update HTTP_UDP_server.py

--- HTTP_UDP_server.py
+++ HTTP_UDP_server.py
@@ -27,27 +27,29 @@ data_to_client += "<html><body>EE-4210: Continuous assessment.<body><html>\r\n\r
             
 #################################### Client conenction and comms ####################################
 def handle_client(client_data, client_address):
-            print(f"\n[NEW CONNECTION] {client_address} connected\n")
-            #data_length = len(client_data)
-            # break pieces by \n
-            #pieces = client_data.decode().split("\n")
-            # immediately blast out message
-            # sendto() used to send to right address since connectionless
-            sent = serversocket.sendto(data_to_client.encode(), client_address)
-            #print(f'sent {sent} bytes back to {client_address}')
+    print(f"\n[NEW CONNECTION] {client_address} connected\n")
+    #data_length = len(client_data)
+    # break pieces by \n
+    #pieces = client_data.decode().split("\n")
 
+    # sendto() used to send to right address since connectionless
+    # UDP blasts out message immeidtaely as it does not verify data
+    sent = serversocket.sendto(data_to_client.encode(), client_address)
+    #print(f'sent {sent} bytes back to {client_address}')
+    
 
 def createserver():
-            print('\nwaiting to receive message\n')
-            while (1):
-                        # UDP connectionless so no listen() & accept()
-                        # data & address received from client
-                         (client_data, client_address) = serversocket.recvfrom(4096)
+    print('\nwaiting to receive message\n')
+    while (1):
+        # UDP connectionless so no listen() & accept()
+        # data & address received from client
+        (client_data, client_address) = serversocket.recvfrom(4096)
         
-                        # New thread started for client --> handle_client
-                        thread = threading.Thread(target=handle_client, args=(client_data, client_address))
-                        thread.start()
-                        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+        # New thread started for client --> handle_client
+        thread = threading.Thread(target=handle_client, args=(client_data, client_address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+
 
 print(f"Access http://{SERVER}:8000")
 createserver()
