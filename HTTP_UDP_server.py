e02ed20f19edec374e470f06a3926436606be35c
Jeevan <78906553+daj337n96@users.noreply.github.com>
2021-04-10 17:42:32 +0800

    Update HTTP_UDP_server.py

--- HTTP_UDP_server.py
+++ HTTP_UDP_server.py
@@ -1,53 +1,65 @@
 from socket import *
 import sys
+import threading
+from datetime import datetime
 
-#SERVER = 'localhost'
+local_dt = datetime.now()
+
+
+#################################### Setup SERVER & PORT ####################################
 SERVER = gethostbyname(gethostname())
 PORT = 8000
+server_address = (SERVER, PORT)
 
-def createserver():
-    # Create a TCP/IP socket
-    sock = socket(AF_INET, SOCK_DGRAM)
-    server_address = (SERVER, PORT)
-    #try:
-    #print(f'starting up on {SERVER} port {PORT}')
-    # Bind the socket to the port
-    sock.bind(server_address)
+serversocket = socket(AF_INET, SOCK_DGRAM)
 
-    while True:
-         # UDP connectionless so no listen() & accept()
-        print('\nwaiting to receive message\n')
-        # data & address received from client
-        client_data, client_address = sock.recvfrom(5000)
-        data_length = len(client_data)
+# Bind the socket to the port
+serversocket.bind(server_address)
 
+#################################### Define FORM & Client reply ####################################
+# HTTP response
+data_to_client = "HTTP/1.1 200 OK\r\n"
+data_to_client += "Date:"+str(local_dt)+"\r\n"
+#data_to_client += "Content-length:"+str(data_length)+"\r\n"
+data_to_client += "Connection: Keep-Alive\r\n"
+data_to_client += "Content-type: text/html; charset=utf-8\r\n"
+data_to_client += "\r\n"
+# actual data response 
+data_to_client += "<html><body>EE-4210: Continuous assessment.<body><html>\r\n\r\n"
+            
+#################################### Client conenction and comms ####################################
+def handle_client(client_data, client_address):
+    print(f"\n[NEW CONNECTION] {client_address} connected\n")
+    connected = True
+    while connected:
+        #data_length = len(client_data)
         # break pieces by \n
-        pieces = client_data.decode().split("\n")
-        # valid data
-        if(len(pieces) > 0):
-            # print out the HTTP GET and favicon
-            # the HTTP GET request houses the word we typed
-            # GET /hello HTTP/1.1
-            print(pieces[0])
-
-        print(f'received {data_length} bytes from {client_address}') 
-        #print(client_data.decode())
-
-        # HTTP response
-        data_to_client = "HTTP/1.1 200 OK\r\n"
-        data_to_client += "content-type: text/html; charset=utf-8\r\n"
-        data_to_client += "\r\n"
-        # actual data response 
-        data_to_client += "<html><body>EE-4210: Continuous assessment.<body><html>\r\n\r\n"
-            
-
-            
+        #pieces = client_data.decode().split("\n")
+                    
         if client_data:
             # sendto() used to send to right address since connectionless
-            sent = sock.sendto(data_to_client.encode(), client_address)
-            print(f'sent {sent} bytes back to {client_address}')
+            sent = serversocket.sendto(data_to_client.encode(), client_address)
+            #print(f'sent {sent} bytes back to {client_address}')
+    
+
+def createserver():
+    print('\nwaiting to receive message\n')
+    while (1):
+        # UDP connectionless so no listen() & accept()
+        # data & address received from client
+        (client_data, client_address) = serversocket.recvfrom(4096)
+        
+        # New thread started for client --> handle_client
+        thread = threading.Thread(target=handle_client, args=(client_data, client_address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() -1}")
+        
+        # valid data
+        #if(len(pieces) > 0):
+            #print(pieces[0])
+
+        #print(f'received {data_length} bytes from {client_address}') 
+        #print(client_data.decode())
 
 print(f"Access http://{SERVER}:8000")
 createserver()
-
-
